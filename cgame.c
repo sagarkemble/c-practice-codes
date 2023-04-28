d5f3d41fff26cf164a2375aec6b71d73ce17e7b0
sagarkemble <131292159+sagarkemble@users.noreply.github.com>
2023-04-28 12:17:15 +0530

    Add files via upload

--- cgame.c
+++ cgame.c
@@ -1,29 +1,32 @@
-#include<stdio.h>
+#include <stdio.h>
 #include<stdlib.h>
 #include<time.h>
-int main(){
-    int entered; 
-    int attempts;
-    srand(time(0));
-    int random =rand()%100+1;
-    printf("%d",random);
-    printf("welcome to cgame");
-    do{
-        
-        scanf("%d",entered);
-        if(entered>random){
-            printf("enter a lower number");
-          
-            }
-              else if(entered<rand){
-                printf("enter a hugher number");
-        }
 
-        attempts++;
-    }
+int main (){
+    printf("-------welcome to c game --------\n\nenter any random and guess number in minimum gusses");
+    int entered_value,random_number,guess,tries,a;
+    int attempts=1;
+    srand(time(0));
+    random_number=rand()%100+1;
+    // printf("%d",random_number);
+//     printf("-------welcome to c game --------\n\npress 1 to play game \npress 2 to exit\n");
+  
     
-while (entered==random);
- printf("you have completed game in %d attempts",attempts);
-    
-}
+ do{
+     scanf("%d",&entered_value);
+     if(entered_value>random_number){
+        printf("enter a lower number\n");
+     }
+     else if(entered_value<random_number){
+        printf("enter a higher number\n");
+     }
+attempts++;
+ }
+while(entered_value!=random_number);
+printf("you have gussed right number in %d attempts\n\n\n\nthank you for playing this game\n\n\n\na code by sagar kemble",attempts);
+return 0;
+   }
+
+
+
 
