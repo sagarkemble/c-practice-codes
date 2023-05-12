d115a0e02c184677f1889a1a782241b671df49da
sagarkemble <131292159+sagarkemble@users.noreply.github.com>
2023-05-12 14:52:22 +0530

    Update array_storer.c

--- array_storer.c
+++ array_storer.c
@@ -1,18 +1,23 @@
 #include<stdio.h>
 int main()
 {
-    int capacity;
-    int array[capacity];
-    int
+int capacity;
+int array[capacity];
 
-    printf("entercapacity of array");
-    scanf("%d",capacity);
-printf("enter a number to be stored in array ");
-for (int i = 0; i <= capacity; i++)
+
+printf("enter capacity of array\n");
+scanf("%d",&capacity);
+for (int i = 0; i < capacity; i++)
 {
- printf("enter a number to stored in array block %d",i+1);
- scanf("%d",)
-   }
+    printf("enter number to be entered in array block %d\n",i+1);
+    scanf("%d",&array[i]);
+    
+}
+for (int i = 0; i < capacity; i++)
+{
+   int *a=&array[i];
+    printf("the value at block %d in array is %d and adress of block is %d\n",i+1,array[i],a);
+}
 
 return 0;
 }
