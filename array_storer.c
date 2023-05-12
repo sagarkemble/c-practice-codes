00623e945f21afb2b5ea19ca90f18f23b8d275ae
sagarkemble <131292159+sagarkemble@users.noreply.github.com>
2023-05-12 10:55:20 +0530

    Add files via upload

---
+++ array_storer.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int capacity;
+    int array[capacity];
+    int
+
+    printf("entercapacity of array");
+    scanf("%d",capacity);
+printf("enter a number to be stored in array ");
+for (int i = 0; i <= capacity; i++)
+{
+ printf("enter a number to stored in array block %d",i+1);
+ scanf("%d",)
+   }
+
+return 0;
+}
